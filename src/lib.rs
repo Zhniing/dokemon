f6b60699dd7e2e8016b040767403ed4a1087435e
Zhniing <690710205@qq.com>
2024-01-31 23:42:50 +0800

    feat: determine the action order by speed

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use std::collections::HashMap;
+use std::process;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
+enum Player {
+    P1,
+    P2,
+}
+
+#[derive(Debug, PartialEq, Clone)]
 struct Hero {
     name: String,
     lv: i32,  // Level
@@ -9,14 +16,24 @@ struct Hero {
     def: i32, // Defense
     spa: i32, // Special Attack
     spd: i32, // Special Defense
+    spe: i32, // Speed
     moves: Vec<String>,
+    owner: Player,
+    id: i32, // Positon
 }
 
 impl Hero {
-    fn new(name: String, lv: i32, base: &[i32], moves: Vec<String>) -> Hero {
+    fn new(
+        name: String,
+        lv: i32,
+        base: &[i32],
+        moves: Vec<String>,
+        owner: Player,
+        id: i32,
+    ) -> Hero {
         // Respectively: hp, att, def, spa, spd
-        let ivs = [31, 31, 31, 31, 31];
-        let evs = [252, 0, 0, 0, 0];
+        let ivs = [31, 31, 31, 31, 31, 31];
+        let evs = [252, 0, 0, 0, 0, 0];
         Hero {
             name,
             lv,
@@ -25,7 +42,10 @@ impl Hero {
             def: calc_stat(base[2], ivs[2], evs[2], lv),
             spa: calc_stat(base[3], ivs[3], evs[3], lv),
             spd: calc_stat(base[4], ivs[4], evs[4], lv),
+            spe: calc_stat(base[5], ivs[5], evs[5], lv),
             moves,
+            owner,
+            id,
         }
     }
 }
@@ -41,38 +61,25 @@ fn calc_stat(base: i32, iv: i32, ev: i32, lv: i32) -> i32 {
 }
 
 /// 龙破斩
-fn dragon_slave(attacker: &Hero, target: &mut Hero) {
+fn dragon_slave(heroes: &mut Vec<Hero>, agent: usize, target: usize) {
     let power = 90;
-    let (att, def) = (attacker.spa, attacker.spd);
-    target.hp -= calc_damage(attacker.lv, att, def, power) as i32;
+    let (att, def) = (heroes[agent].spa, heroes[target].spd);
+    heroes[target].hp -= calc_damage(heroes[agent].lv, att, def, power) as i32;
 }
 
 /// 远程攻击
-fn ranged_attack(attacker: &Hero, target: &mut Hero) {
+fn ranged_attack(heroes: &mut Vec<Hero>, agent: usize, target: usize) {
     let power = 50;
-    let (att, def) = (attacker.att, attacker.def);
-    target.hp -= calc_damage(attacker.lv, att, def, power) as i32;
+    let (att, def) = (heroes[agent].att, heroes[target].def);
+    heroes[target].hp -= calc_damage(heroes[agent].lv, att, def, power) as i32;
 }
 
 fn calc_damage(lv: i32, att: i32, def: i32, power: i32) -> f64 {
     (2.0 * lv as f64 + 10.0) / 250.0 * (att as f64 / def as f64) * power as f64 + 2.0
 }
 
-fn run_move(
-    hero1: &Hero,
-    hero2: &mut Hero,
-    moves: &HashMap<String, fn(&Hero, &mut Hero)>,
-    move_sel: &String,
-) {
-    match moves.get(move_sel) {
-        Some(m) => m(hero1, hero2),
-        None => (),
-    };
-}
-
-// Init moves
-fn init_moves() -> HashMap<String, fn(&Hero, &mut Hero)> {
-    let mut moves: HashMap<String, fn(&Hero, &mut Hero)> = HashMap::new();
+fn init_moves() -> HashMap<String, fn(&mut Vec<Hero>, usize, usize)> {
+    let mut moves: HashMap<String, fn(&mut Vec<Hero>, usize, usize)> = HashMap::new();
 
     moves.insert("龙破斩".to_string(), dragon_slave);
     moves.insert("远程攻击".to_string(), ranged_attack);
@@ -80,53 +87,83 @@ fn init_moves() -> HashMap<String, fn(&Hero, &mut Hero)> {
     moves
 }
 
-pub fn run() {
-    let moves = init_moves();
+fn init_test_heroes(lv: i32, base: ([i32; 6], [i32; 6])) -> Vec<Hero> {
+    let (p1, p2) = (Player::P1, Player::P2);
 
-    let lv = 50;
-    let base_lina = [85, 60, 65, 130, 75, 98];
     let moves_lina1 = vec!["龙破斩".to_string(), "远程攻击".to_string()];
-    let mut hero1 = Hero::new("火女1".to_string(), lv, &base_lina, moves_lina1);
+    let hero1 = Hero::new("火女1".to_string(), lv, &base.0, moves_lina1, p1, 0);
     println!("英雄1: {:?}", hero1);
+
     let moves_lina2 = vec!["龙破斩".to_string(), "远程攻击".to_string()];
-    let mut hero2 = Hero::new("火女2".to_string(), lv, &base_lina, moves_lina2);
+    let hero2 = Hero::new("火女2".to_string(), lv, &base.1, moves_lina2, p2, 1);
     println!("英雄2: {:?}", hero2);
 
+    vec![hero1, hero2]
+}
+
+fn print_battle_log(agent: &Hero, target: &Hero, hp_orig: i32, move_sel: &String) {
+    print!(
+        "【{}】对【{}】使出【{}】",
+        agent.name, target.name, move_sel
+    );
+    print!(", 造成了【{}】点伤害", hp_orig - target.hp);
+    println!(",【{}】的体力: {} -> {}", target.name, hp_orig, target.hp);
+}
+
+fn select_target(id: i32, heroes: &Vec<Hero>) -> i32 {
+    assert!(heroes.len() > 1);
+    assert!(id < heroes.len() as i32);
+    if heroes.len() == 2 {
+        id ^ 1 // 0 or 1
+    } else {
+        eprintln!("Not implemented yet!");
+        process::exit(1);
+    }
+}
+
+fn select_move(agent: &Hero) -> String {
+    let move_index = agent.id as usize;
+    agent.moves[move_index].clone() // The move selected to use
+}
+
+fn run_battle(mut heroes: Vec<Hero>, moves: &HashMap<String, fn(&mut Vec<Hero>, usize, usize)>) {
     // Start
-    let mut round = 1;
+    let mut round = 0;
     loop {
+        round += 1;
         println!("\n---------- 第{}回合 ----------\n", round);
 
-        let move_sel = &hero1.moves[1]; // The move selected to use
-        let hp_orig = hero2.hp; // The hp before executing the move
-        run_move(&hero1, &mut hero2, &moves, move_sel);
+        let mut action_order = vec![0, 1];
+        while action_order.len() > 0 {
+            action_order.sort_by_key(|id| heroes[*id as usize].spe);
 
-        // Battle log
-        print!("【{}】对【{}】使出【{}】", hero1.name, hero2.name, move_sel);
-        print!(", 造成了【{}】点伤害", hp_orig - hero2.hp);
-        println!(",【{}】的体力: {} -> {}", hero2.name, hp_orig, hero2.hp);
+            let agent = action_order.pop().unwrap();
+            let move_sel = select_move(&heroes[agent]);
+            let target = select_target(heroes[agent].id, &heroes) as usize;
+            let hp_orig = heroes[target].hp; // The hp before executing the move
 
-        // Check survival
-        if hero2.hp <= 0 {
-            break;
+            // TODO: perform moves after select_move() of both players
+            moves.get(&move_sel).unwrap()(&mut heroes, agent, target);
+
+            // Battle log
+            print_battle_log(&heroes[agent], &heroes[target], hp_orig, &move_sel);
+
+            // Check survival
+            if heroes[target].hp <= 0 {
+                return;
+            }
         }
-
-        let move_sel = &hero2.moves[0]; // The move selected to use
-        let hp_orig = hero1.hp; // The hp before executing the move
-        run_move(&hero2, &mut hero1, &moves, move_sel);
-
-        // Battle log
-        print!("【{}】对【{}】使出【{}】", hero2.name, hero1.name, move_sel);
-        print!(", 造成了【{}】点伤害", hp_orig - hero1.hp);
-        println!(",【{}】的体力: {} -> {}", hero1.name, hp_orig, hero1.hp);
-
-        // Check survival
-        if hero1.hp <= 0 {
-            break;
-        }
-
-        round += 1;
     }
+}
+
+pub fn run() {
+    // Init
+    let moves = init_moves();
+    let lv = 50;
+    let base_lina = ([85, 60, 65, 130, 75, 98], [85, 60, 65, 130, 75, 99]);
+    let heroes = init_test_heroes(lv, base_lina);
+
+    run_battle(heroes, &moves)
 }
 
 #[cfg(test)]
@@ -136,5 +173,18 @@ mod tests {
     #[test]
     fn test_run() {
         run();
+    }
+
+    #[test]
+    fn test_select_target() {
+        let lv = 50;
+        let base = ([50, 50, 50, 50, 50, 50], [50, 50, 50, 50, 50, 49]);
+        let heroes = init_test_heroes(lv, base);
+
+        let target = select_target(0, &heroes);
+        assert_eq!(target, 1);
+
+        let target = select_target(1, &heroes);
+        assert_eq!(target, 0);
     }
 }
