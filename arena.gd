7dafb9499775b4e0a46253b9cef37b2a21a973c7
Zhniing <690710205@qq.com>
2024-07-30 17:46:36 +0800

    feat: determine the order of heroes by their speed

--- arena.gd
+++ arena.gd
@@ -1,6 +1,9 @@
 extends Control
 
 
+var commands = {}
+
+
 func _ready():
 	var P1 = $Player1
 	var P2 = $Player2
@@ -12,7 +15,9 @@ func _ready():
 	# Bind move buttons
 	var btns = $Player1/MoveBtns
 	for btn in btns.get_children():
-		btn.connect("pressed", Callable(btns, "attack").bind(P1, P2))
+		btn.connect("pressed", func():
+			commands[P1.name] = Callable(btns, "attack").bind(P1, P2)
+		)
 
 	# Inital HP bar
 	hp = $Player2/HP
@@ -21,4 +26,20 @@ func _ready():
 	# Bind move buttons
 	btns = $Player2/MoveBtns
 	for btn in btns.get_children():
-		btn.connect("pressed", Callable(btns, "attack").bind(P2, P1))
+		btn.connect("pressed", func():
+			commands[P2.name] = Callable(btns, "attack").bind(P2, P1)
+		)
+
+func _process(delta):
+	if commands.size() == 2:
+		# Sort by the speed
+		var order = commands.keys()
+		order.sort_custom(func(a, b):
+			return get_node(a).hero.stats[5] > get_node(b).hero.stats[5]
+		)
+
+		# Execute the commands
+		for i in order:
+			commands[i].call()
+
+		commands.clear()
--- player.gd
+++ player.gd
@@ -3,3 +3,7 @@ extends Control
 
 var base = [85, 60, 65, 130, 75, 98]
 var hero = Hero.new(50, base)
+
+func _ready():
+	if name == "Player2":
+		hero.stats[5] += 1  # TEST
